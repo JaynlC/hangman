e07060f01fceefdd4aad2bbb219cdcef359706e6
Jay C <jc30g14@soton.ac.uk>
2022-11-05 00:44:32 +0000

    added option to start new game. Refactored Player class methods for cleaner code. Added more interactivity for player via messages module.

--- hangman.rb
+++ hangman.rb
@@ -7,30 +7,56 @@ module Messages
     puts "\n"
   end
 
+  def seperator_line
+    puts "----------------------------------------------"
+  end
+
   def greeting(player)
     new_line()
     puts "Welcome #{player} to Hangman!"
+    new_line()
   end
 
   def pick_letter
+    seperator_line()
     puts "Pick one letter as your guess to find the word"
   end
 
   def display_player_guess(player_guess)
     new_line()
     puts "This is your guess: #{player_guess}"
-    new_line()
   end
 
-  def display_letter_match_message(match_logic)
+  def display_letter_match_message(match_logic, letter)
+    new_line()
     if match_logic
-      puts "Letter is a Match! Well done"
-    else puts "No match, unlucky!"
+      puts "#{letter} is a Match! Well done"
+    else puts "#{letter} is not a match!"
     end
   end
 
   def display_guesses_remaining(number)
+    new_line()
     puts "Guesses remaining: #{number}"
+  end
+
+  def display_all_guesses(guesses)
+    new_line()
+    puts "Guesses so far: #{guesses}"
+  end
+
+  def display_new_game_option
+    new_line()
+    seperator_line()
+    puts "Please type 'R' to restart the game, or 'N' to end the game."
+  end
+
+  def end_game_message(name)
+    seperator_line()
+    new_line()
+    puts "Thank you #{name} for playing Hangman!"
+    new_line()
+    seperator_line()
   end
 end
 
@@ -55,18 +81,19 @@ end
 class Player
   include Messages
 
-  attr_accessor :player_guess_letters, :letter_match
+  attr_accessor :player_guess_letters, :letter_match, :player_letter_choices
 
   def initialize()
     puts ask_player_name()
     @name = gets.chomp.capitalize
     greeting(@name)
+    @player_letter_choices = []
   end
 
   def player_guess_display_letters(word_answer)
     @word_answer_length = word_answer.strip.length
     @player_guess_letters = Array.new(@word_answer_length).map {|letter| letter = "_" }
-    puts "The word you should guess has #{@word_answer_length} letters."
+    puts "The word you should guess has #{@word_answer_length} letters. Good Luck!"
     display_player_guess(player_guess_letters)
   end
 
@@ -76,11 +103,14 @@ class Player
       pick_letter()
       @player_guess_one_letter = gets.chomp.upcase
       if @player_guess_one_letter.length == 1 && @player_guess_one_letter =~ /[A-Za-z]/
+        @player_letter_choices.push(@player_guess_one_letter)
         @picked_letter = true
-        # add to guessed letter array. If same letter picked give message. 
       end
     end
-    #break into further methods
+    check_player_guess(word_answer_array)
+  end
+
+  def check_player_guess(word_answer_array)
     @letter_match = false
     word_answer_array.each_with_index do |letter_answer, index_answer|
       if letter_answer == @player_guess_one_letter
@@ -88,7 +118,8 @@ class Player
         @letter_match = true
       end
     end
-    display_letter_match_message(letter_match)
+    display_letter_match_message(letter_match, @player_guess_one_letter)
+    display_all_guesses(@player_letter_choices)
     display_player_guess(player_guess_letters)
   end
 
@@ -99,6 +130,10 @@ class Player
   def return_check_letter_match
     letter_match
   end
+
+  def return_name
+    @name
+  end
 end
 
 class ComputerPlayer
@@ -107,7 +142,6 @@ class ComputerPlayer
   def random_word_generator(words)
     @random_index = rand(words.length)
     @comp_word_guess = words[@random_index].upcase
-    puts "This is the random word: #{@comp_word_guess}" #troubleshooting
     @comp_word_guess
   end
 
@@ -165,10 +199,29 @@ class Game
 
   def game_end
     if check_win()
+      new_line()
       puts "You Win!"
     elsif @@count_guess == 0
-      puts "You Lose, Better Luck Next Time"
-    end 
+      new_line()
+      puts "You Lost, the hidden word was #{@computer_word_guess}"
+      puts "Better Luck Next Time!"
+    end
+    new_game() 
+  end
+
+  def new_game
+    @option_selected = false
+    until @option_selected
+      display_new_game_option()
+      @new_game_choice = gets.chomp.upcase
+      if @new_game_choice == "R"
+        initialize()
+        @option_selected = true
+      elsif @new_game_choice == "N"
+       end_game_message(player.return_name())
+       @option_selected = true
+      end
+    end
   end
 end
 
